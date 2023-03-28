91d32b9d1aaaa1c95a62b9101539df507ca24050
Yuval Gal <yuvalgal017@gmail.com>
2023-03-28 20:57:51 +0300

    stupid code

--- emit.js
+++ emit.js
@@ -1,43 +1,30 @@
+const { SerialPort } = require("serialport");
 
-const { SerialPort } = require('serialport')
-
- const port = "COM5";
- const staringDegree = 25;
- const endingDegree = 255;
+const port = "COM3";
+const staringDegree = 25;
+const endingDegree = 255;
 // every 1.2777 is one degree
- function connectToChip() {
+function connectToChip() {
   // create connection to serial port
   const serialPort = new SerialPort({
-    path:port,
-    baudRate:9600,
+    path: port,
+    baudRate: 9600,
   });
   return serialPort;
 }
 
- function sendMessage(dg,serialPort) {
-  const buffer = new Buffer([ dg]);
-  serialPort.write(buffer, (err) => {
-    if (err) {
-      return console.log("Error on write: ", err.dg);
-    }
-    console.log(dg);
+function sendMessage(payload) {
+  let serialPort = connectToChip();
+  if (!serialPort) return;
+  serialPort.on("open", () => {
+    const data = Buffer.from(payload);
+    serialPort.write(data, undefined, () => {
+      serialPort.close();
+    });
   });
 }
 
- function linarMoveToEnd() {
-let linarCurrentDegree = staringDegree;
-  const linarInterval = setInterval(() => {
-    if (linarCurrentDegree <= endingDegree) {
-      sendMessage(linarCurrentDegree);
-      linarCurrentDegree++;
-    }
-    else {
-      clearInterval(linarInterval);
-    }
-  }, 4);
-}
 module.exports = {
-  linarMoveToEnd,
   sendMessage,
-  connectToChip
-}
+  connectToChip,
+};
--- listener.js
+++ listener.js
@@ -1,19 +1,27 @@
-const emit = require("./emit.js")
-const http = require('http');
-var express = require('express')
-var cors = require('cors')
-const hostname = '127.0.0.1';
+const emit = require("./emit.js");
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const hostname = "127.0.0.1";
 const port = 4567;
 
-const app = express()
+const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.post('', function (req, res, next) {
-    console.log(req.body);
-    res.json(req.body)
-})
+app.post("", function (req, res, next) {
+  const data = req.body.data;
+  const payload = [data.x, data.y];
+  console.log(payload);
+  emit.sendMessage(payload);
+  res.end();
+});
 
 app.listen(4567, function () {
-    console.log('CORS-enabled web server listening on port 80')
-})
+  console.log("CORS-enabled web server listening on port 80");
+});
+
+process.on("uncaughtException", function (err) {
+  console.error(err);
+  // console.log("Node NOT Exiting...");
+});
